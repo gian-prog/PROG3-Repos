3661395c477c331132f10a7f589622459139a28f
Giancarlo Benedetti <6262557@cegepmontpetit.ca>
2024-08-29 10:30:11 -0400

    Moyenne - Partie 1

---
+++ Moyenne/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package org.giancarlo
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+}
+fun creeTableauAleatoire(n: Int): Array<Int> {
+    var num = mutableListOf<Int>().random()
+    return num ; 
+}
