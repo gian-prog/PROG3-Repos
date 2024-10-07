c83364214e1200a49e8854e001b53c41408dfc7c
Giancarlo Benedetti <6262557@cegepmontpetit.ca>
2024-10-07 14:06:21 -0400

    Progress: AvancementProg3

---
+++ ANDROID/EXERCICES/BarreApp/app/src/main/java/com/benedetti/ArticleActivity.kt
@@ -0,0 +1,15 @@
+package com.benedetti
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_article)
+    }
+}
--- ANDROID/EXERCICES/BarreApp/app/src/main/java/com/benedetti/MainActivity.kt
+++ ANDROID/EXERCICES/BarreApp/app/src/main/java/com/benedetti/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.benedetti
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,5 +18,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.btnArticle.setOnClickListener {
+            // On crée l'intent, qui a besoin :
+            // 1. Du contexte actuel
+            // 2. De la classe d'activité à démarrer.
+            val intent = Intent(this, ArticleActivity::class.java)
+
+            // On démarre l'activité en lui passant l'intent qu'on vient de créer.
+            startActivity(intent)
+        }
         }
     }
