959d6a5b942893ab6802bfaa7b6cdd24ece3c44e
Giancarlo Benedetti <6262557@cegepmontpetit.ca>
2024-12-16 14:57:07 -0500

    FCT: Fin Formatif

--- ANDROID/FORMATIF/Q2/Formatif/app/src/main/java/com/giancarlo/formatif/adapters/VersionAdapter.kt
+++ ANDROID/FORMATIF/Q2/Formatif/app/src/main/java/com/giancarlo/formatif/adapters/VersionAdapter.kt
@@ -1,10 +1,12 @@
 package com.giancarlo.formatif.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.giancarlo.formatif.DetailsActivity
 import com.giancarlo.formatif.databinding.VersionItemBinding
 import com.giancarlo.formatif.models.Version
 
@@ -15,6 +17,13 @@ class VersionAdapter : ListAdapter<Version, VersionAdapter.VersionItemViewHolder
         fun bind(version: Version) {
             binding.version.text = version.version
             binding.nomVersion.text = version.nomVersion
+            binding.root.setOnClickListener {
+                val myIntent = Intent(binding.root.context, DetailsActivity::class.java)
+                // On ajoute le nom de l'élément(la question) à l'intent
+                myIntent.putExtra("version", version.version)
+                myIntent.putExtra("nomVersion", version.nomVersion)
+                binding.root.context.startActivity(myIntent)
+            }
         }
     }
 
